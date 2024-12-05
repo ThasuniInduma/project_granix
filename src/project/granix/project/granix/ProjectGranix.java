3f1e96d8942a50c8201d450b9d6738b00e8beaab
Thasuni Induma <138390612+ThasuniInduma@users.noreply.github.com>
2024-12-05 11:10:01 +0530

    not completed

--- src/project/granix/project/granix/ProjectGranix.java
+++ src/project/granix/project/granix/ProjectGranix.java
@@ -29,7 +29,7 @@ public class ProjectGranix {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        new GovernmentAvailability().setVisible(true);
+        new HomeScreen().setVisible(true);
     }
     
 }
